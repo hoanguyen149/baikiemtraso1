65b9c01308b25ea4440ec697d847daa950658586
hoanguyen149 <0308191133@caothang.edu.vn>
2022-05-17 09:49:33 +0700

    baikiemtraso1

---
+++ app/src/main/java/com/example/baikiemtraso1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.baikiemtraso1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+     Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
